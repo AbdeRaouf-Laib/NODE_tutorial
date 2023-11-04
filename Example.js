06109acf0cc2425236d39b24c9f29fcfa005a768
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-11-04 15:05:05 +0100

    add examples

--- Example.js
+++ Example.js
@@ -1,1 +1,53 @@
-// init
+/******************command*****************/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/*******************rescorce********************/
+/*1-) Website: coding addict                   */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/****************Necessary tools****************/
+/*GLOBALS  - NO WINDOW!!!! */
+/*  __dirname -path to current directory*/
+/* __filename -file name*/
+/* require    -function to use modules (CommonJS)*/
+/* module     -info about current module (file)*/
+/* process    -info about env where thr program is being excuted*/
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+
+const { log } = require("console")
+
+/*******************Example**********************/
+const amount = 12
+
+if (amount < 12){
+    console.log("amounte inf then 12")
+}else{
+    console.log('amounte sup then 12')
+}
+console.log(`Hay!!`)
+console.log(__dirname)
